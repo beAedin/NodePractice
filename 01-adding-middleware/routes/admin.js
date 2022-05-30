09eb107bab8b5599a0b37aad1bf0076ecff06130
beAedin <panda01com@g.yju.ac.kr>
2022-05-30 20:14:59 +0900

    End : middleware    START : HTML template engine 'pug'

--- 01-adding-middleware/routes/admin.js
+++ 01-adding-middleware/routes/admin.js
@@ -5,8 +5,12 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+// admin/add-product
+// app:12 ---> app.use("/admin", adminRoutes);
+//
+
 router.get("/add-product", (req, res, next) => {
-    // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    //res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
     res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
