5a481d89190916126883298c2c9bfd09473ae7f1
beAedin <panda01com@g.yju.ac.kr>
2022-06-13 20:16:18 +0900

    MVC Model... Input data to an Array

--- mvc/models/product.js
+++ mvc/models/product.js
@@ -1,24 +1,37 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        const p = path.join(
-            dirname(require.main.filename),
-            "data",
-            "products.json"
-        );
-        fs.readFile(p, (err, fileContent) => {
-            console.log(fileContent);
-        });
-        products.push(this);
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
